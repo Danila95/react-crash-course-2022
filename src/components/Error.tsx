136c2329171c23133df6678cc051f09c1bc5b080
ddb4 <ddb4@tpu.ru>
2022-09-05 11:50:22 +0700

    finished project

---
+++ src/components/Error.tsx
@@ -0,0 +1,10 @@
+
+interface ErrorProps {
+    error: string
+}
+
+const Error = ({ error }: ErrorProps) => {
+    return ( <p className='text-center text-red-600'>{ error }</p> );
+};
+
+export default Error;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil illum soluta sint ipsum blanditiis reiciendis minima quis quia ut. Ullam rerum tempora, sapiente ipsam accusamus debitis, voluptatum molestiae cumque aperiam expedita facilis similique maxime, officiis possimus? Quisquam atque maxime inventore id! Nesciunt voluptatum repellat accusantium totam sit neque laudantium placeat.
+        </div>
+    );
+};
+
+export default AboutPage;
